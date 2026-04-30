3c8a40a5c6c75f4a318a2a7f0ae5d0c299d1d253
Sukumar <sukumar.a7230@gmail.com>
2026-04-30 16:32:04 -0500

    added exception handler for catching null or invalid values in deposit or withdraw

--- bankapp/src/main/java/com/banking/bankapp/exception/GlobalExceptionHandler.java
+++ bankapp/src/main/java/com/banking/bankapp/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.banking.bankapp.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,5 +18,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> invalidAmount(InvalidAmountException ex){
         ErrorResponse error = new ErrorResponse(400,ex.getMessage());
         return ResponseEntity.status(400).body(error);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex){
+    ErrorResponse error = new ErrorResponse(400,"Invalid request body");
+    return ResponseEntity.status(400).body(error);
     }
 }
